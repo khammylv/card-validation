72ae9061f88472ed86a9444e4974b82a883326e1
camila <khammylv@gmail.com>
2022-09-27 22:41:51 -0500

    algoritmo terminado con pruebas listas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import validator from './validator.js';
+const formCard = document.getElementById("form-card");
 
-console.log(validator);
+
+
+
+
+formCard.addEventListener("submit", e=>{
+    e.preventDefault();
+   
+    const entrada = document.getElementById("input-card")
+    const mensajeCard = document.getElementById("mensaje-card");
+    const cardNews = entrada.value;
+    const numVal = /\D/g;
+    const valnum = numVal.test(cardNews);
+    if (cardNews === "" ||valnum == true ) {
+        mensajeCard.innerHTML = "Escribe mensaje valido";
+        entrada.value = "";
+      } else {
+             let entradaArray = cardNews;
+             validator.isValid(entradaArray)
+             validator.maskify(entradaArray)
+             entrada.value = ""; 
+              
+        
+      }
+});
+
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,72 @@
 const validator = {
-  // ...
+  isValid : function(creditCardNumber){
+    const cardNew = Array.from(creditCardNumber)
+    const cards =  cardNew.reverse();
+    let cardPares = cards.filter((num, index) =>index % 2 != 0);
+    let cardNum = cards.filter((num, index) =>index % 2 == 0);
+    let dobles = cardPares.map((element) => element * 2);
+    let multCards = [];
+     
+        for (let doble of dobles) {
+         if(doble >=10){
+             multCards.push(doble);
+         }else{
+           cardNum.push(doble);
+         }
+        }
+       
+       
+         for (let i = 0; i < multCards.length; i++) {
+           if (multCards[i] >=10){
+            
+             multCards[i] = parseInt(String(multCards[i]).charAt(0)) +  parseInt(String(multCards[i]).charAt(1))
+             cardNum.push(multCards[i])
+           }
+           
+          }
+
+        let sumaTotal = 0;
+        for (let i = 0; i < cardNum.length; i++){
+            sumaTotal += parseInt(cardNum[i]);
+          }
+        
+         
+        sumaTotal = sumaTotal % 10;
+        
+         if (sumaTotal === 0){
+           return true;
+         }else{
+           return false;
+         }
+         
+ 
+
+},
+
+maskify : function (creditCardNumber) {
+  let validar = validator.isValid(creditCardNumber);
+  const cardEntrada = Array.from(creditCardNumber)
+  let cambio = cardEntrada;
+  let oculto = []
+ 
+ for (let i = 0; i < cambio.length; i++){
+    oculto.push('#')
+}
+let letras = cardEntrada.slice(- 4)
+let resulFinal = oculto.slice(0,- 4);
+ let valido = resulFinal.join('') + letras.join('')
+ let invalido = resulFinal.join('') +letras.join('')
+
+  if(validar == true){ 
+    return valido
+    
+  }else{
+    return invalido
+   
+  }
+  
+
+}
 };
 
 export default validator;
